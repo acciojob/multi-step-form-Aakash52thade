c5510b54c6c7d2944aa0789b40c3eeb77557c87b
akash bawantahde <akashbawanthade02@gmail.com>
2025-11-25 12:17:58 +0530

    added

--- src/components/App.js
+++ src/components/App.js
@@ -11,42 +11,44 @@ const App = () => {
     car_price:"", 
     card_info:"", 
     expiry_date:""
-  });
+  })
 
   const [currentStep, setCurrentStep] = useState(1);
 
   const handleNext = () => {
-    if (currentStep < 3) {
+    if(currentStep < 3){
       setCurrentStep(currentStep + 1);
     }
-  };
+  }
 
   const handlePrevious = () => {
-    if (currentStep > 1) {
-      setCurrentStep(currentStep - 1);
+    if(currentStep > 1){
+      setCurrentStep(currentStep -1);
     }
-  };
+  }
 
   const handleInputChange = (name, value) => {
-    setFields(prev => ({ ...prev, [name]: value }));
-  };
+    setFields({
+      ...fields, [name]: value
+    })
+  }
 
   const handleSubmit = () => {
-    console.log("Form Submitted:", fields); // ONLY RUNS ON SUBMIT
-  };
-
+    console.log("Form Submitted:", fields);
+  }
+  
   return (
-    <div>
-      <Step
-        fields={fields}
-        handleInputChange={handleInputChange}
-        handleSubmit={handleSubmit}
-        handlePrevious={handlePrevious}
-        currentStep={currentStep}
-        handleNext={handleNext}
-      />
+    <div className="container">   {/* <-- ADDED CLASSNAME */}
+       <Step 
+          fields={fields} 
+          handleInputChange={handleInputChange} 
+          handleSubmit={handleSubmit}  
+          handlePrevious={handlePrevious} 
+          currentStep={currentStep} 
+          handleNext={handleNext}
+       />
     </div>
-  );
+  )
 }
 
 export default App;
